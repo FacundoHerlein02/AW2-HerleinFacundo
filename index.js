a3b3efd0fcbc73c2ea0eafe99be23b3443bf09df
Facu <herleinfacundo@gamil.com>
2025-04-13 14:54:04 -0300

    detalle de las ventas

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { readFile } from 'fs/promises';
 
 const file = await readFile('./JSON/productos.json', 'utf-8');
+const file2 = await readFile('./JSON/usuarios.json', 'utf-8');
+const file3 = await readFile('./JSON/ventas.json', 'utf-8');
 const motos= JSON.parse(file);
+const usuarios= JSON.parse(file2);
+const ventas= JSON.parse(file3);
 //console.log(motos);
 console.log("MOTO 2")
 console.log(motos[2].marca)
@@ -45,3 +49,29 @@ const get_rango = (min,max)=>{
 }
 console.log("RANGO DE PRECIOS")
 console.log(get_rango(30000,120000))
+
+//Muestra detalle de venta y todos los datos
+const get_detalleVenta=(id) =>{
+    let venta= ventas.find(e => e.id_venta == id);    
+    let filtro_producto= venta.productos.map(e =>
+    { 
+        let prod= motos.find(p=> p.id_producto == e.id_producto);
+        return {
+            marca: prod.marca,
+            descripcion: prod.descripcion,
+            cantidad: e.cantidad                
+        };
+    });
+    let cliente = usuarios.find(u => u.id_cliente == venta.id_cliente);
+    let filtro_cliente = {
+        nombre: cliente.Nombre, 
+        apellido: cliente.Apellido 
+    };
+    return {
+        fecha: venta.fecha,
+        cliente: filtro_cliente,
+        productos: filtro_producto
+    };   
+}
+console.log("DETALLE DE VENTAA")
+console.log(get_detalleVenta(2))
